597e08bdf6857bfb246ff2f9045a76c087dd7b6d
Tomek O <34099068+tomeko74@users.noreply.github.com>
2019-12-05 20:31:41 +0100

    24 Install Apache on EC2

--- code/ec2-fundamentals/ec2-basics.sh
+++ code/ec2-fundamentals/ec2-basics.sh
@@ -7,9 +7,23 @@
 # get admin privileges
 sudo su
 
-# install httpd (Linux 2 version)
+# system update, -y is yes for every question
 yum update -y
+
+# install httpd (Linux 2 version)
 yum install -y httpd.x86_64
+
+# starting service httpd
 systemctl start httpd.service
+
+# enable this service again after reboot
 systemctl enable httpd.service
+
+# display initial page
+curl localhost:80
+
+# first page
+echo "Hello World" > /var/www/html/index.html
+
+# page with hostname
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
